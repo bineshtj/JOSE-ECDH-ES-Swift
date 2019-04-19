330dcd3ebba2929e854ce46c9e2c1dbbc586e5bd
MFantcy <mfantcy@gmail.com>
2019-04-20 04:40:52 +0800

    Indentation

--- ECDHESSwift/Classes/JWE/JWE.swift
+++ ECDHESSwift/Classes/JWE/JWE.swift
@@ -9,23 +9,21 @@ import Foundation
 import JOSESwift
 
 public protocol JSONWebEncryption {
-    
-    var header: JSONWebEncryptionHeader {get}
-    
-    var encryptedKey: Data {get}
-    
-    var initializationVector: Data {get}
-    
-    var ciphertext: Data {get}
-    
-    var authenticationTag: Data {get}
-    
-    var compactSerializedString: String {get}
-    
-    var compactSerializedData: Data {get}
-    
-    var additionalAuthenticatedData: Data {get}
-    
+    var header: JSONWebEncryptionHeader { get }
+
+    var encryptedKey: Data { get }
+
+    var initializationVector: Data { get }
+
+    var ciphertext: Data { get }
+
+    var authenticationTag: Data { get }
+
+    var compactSerializedString: String { get }
+
+    var compactSerializedData: Data { get }
+
+    var additionalAuthenticatedData: Data { get }
+
     func decrypt(key: JWK) throws -> Data
 }
-
--- ECDHESSwift/Classes/JWE/JWEHeader.swift
+++ ECDHESSwift/Classes/JWE/JWEHeader.swift
@@ -10,11 +10,18 @@ import JOSESwift
 
 public protocol JSONWebEncryptionHeader {
     var alg: String { get }
+
     var enc: String { get }
+
     var epk: ECPublicKey? { get }
+
     var apu: String? { get }
+
     var apv: String? { get }
+
     subscript(index: String) -> Any? { get }
+
     func jsonSerializedData() -> Data
+
     func allParameters() -> [String: Any]
 }
