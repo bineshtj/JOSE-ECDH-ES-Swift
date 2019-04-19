2c7f3c4b892a4138b398509f4d972ede493723e7
MFantcy <mfantcy@gmail.com>
2019-04-20 04:44:37 +0800

    "zip" DEF support

--- ECDHESSwift/Classes/JWE/EcdhEsEncryptor.swift
+++ ECDHESSwift/Classes/JWE/EcdhEsEncryptor.swift
@@ -16,46 +16,45 @@ class EcdhEsEncryptor: JWEEncryptor {
         _ pubKey: ECPublicKey,
         _ apu: Data,
         _ apv: Data
-        ) throws -> Data = ecdhKeyAgreementCompute
-    
+    ) throws -> Data = ecdhKeyAgreementCompute
+
     internal var getRandomBytes: (_ size: Int) -> Data = randomBytes
-    
+
     internal var createECKeyPair: (_ curveType: ECCurveType) throws -> ECKeyPair = generateECKeyPair
-    
+
     internal func guardAlgorithms(_ header: JSONWebEncryptionHeader) throws -> (EcdhEsAlgorithm, EncryptionAlgorithm) {
         guard let alg = EcdhEsAlgorithm(rawValue: header.alg) else {
             throw ECDHEESError.unknownOrUnsupportedAlgorithm(reason: "alg: \(header.alg)")
         }
-        
+
         guard let enc = EncryptionAlgorithm(rawValue: header.enc) else {
             throw ECDHEESError.unknownOrUnsupportedAlgorithm(reason: "enc: \(header.enc)")
         }
         return (alg, enc)
     }
-    
+
     func encrypt(
         plaintext: Data,
         key: JWK,
         header: JSONWebEncryptionHeader,
         options: [String: Any] = [:]
-        ) throws -> (header: JSONWebEncryptionHeader, encryptedKey: Data, iv: Data, ciphertext: Data, tag: Data) {
-        
+    ) throws -> (header: JSONWebEncryptionHeader, encryptedKey: Data, iv: Data, ciphertext: Data, tag: Data) {
         let (alg, enc) = try guardAlgorithms(header)
-        
+
         guard let staticPubKey = key as? ECPublicKey else {
             throw ECDHEESError.invalidJWK(reason: "key must be an ECPublicKey")
         }
-        
+
         var ephemeralKeyPair: ECKeyPair
         if let eKeyPair = options["ephemeralKeyPair"] as? ECKeyPair {
             ephemeralKeyPair = eKeyPair
         } else {
             ephemeralKeyPair = try createECKeyPair(staticPubKey.crv)
         }
-        
+
         let apu = Data(base64Encoded: header.apu ?? "") ?? Data()
         let apv = Data(base64Encoded: header.apv ?? "") ?? Data()
-        
+
         let kek = try keyAgreementCompute(alg, enc, ephemeralKeyPair.getPrivate(), staticPubKey, apu, apv)
         var cek: Data, encryptedKey: Data
         if let keyWrapAlgorithm = alg.keyWrapAlgorithm {
@@ -69,27 +68,35 @@ class EcdhEsEncryptor: JWEEncryptor {
             cek = kek
             encryptedKey = Data()
         }
-        
+
         let iv = options["iv"] as? Data ?? getRandomBytes(enc.ivSize / 8)
-        
-        // ZIP
-        
+
+        var dataToEnc = plaintext
+        if header["zip"] != nil {
+            guard
+                let zip = header["zip"] as? String,
+                let zipAlg = JSONWebEncryptionCompressionAlgorithm(rawValue: zip) else {
+                throw ECDHEESError.unknownOrUnsupportedCompressionAlgorithm(reason: header["zip"] as? String ?? "non String")
+            }
+            dataToEnc = try zipAlg.compress(plaintext)
+        }
+
         var resHeader = try EcdhEsJweHeader(cloneFrom: header)
         if header.epk == nil || !ephemeralKeyPair.getPrivate().isCorrespondWith(header.epk!) {
             resHeader.epk = ephemeralKeyPair.getPublic()
         }
-        
+
         var aad = resHeader.jsonSerializedData().base64URLEncodedData()
         if let extAad = options["aad"] as? Data {
             aad += ".".data(using: .ascii)! + extAad
         }
         aad += options["aad"] as? Data ?? Data()
-        
-        let (ciphertext, tag) = try enc.encrypt(plaintext: plaintext, key: cek, iv: iv, tagLen: enc.tagLength / 8, aad: aad)
-        
+
+        let (ciphertext, tag) = try enc.encrypt(plaintext: dataToEnc, key: cek, iv: iv, tagLen: enc.tagLength / 8, aad: aad)
+
         return (resHeader, encryptedKey, iv, ciphertext, tag)
     }
-    
+
     func decrypt(
         key: JWK,
         header: JSONWebEncryptionHeader,
@@ -98,36 +105,42 @@ class EcdhEsEncryptor: JWEEncryptor {
         ciphertext: Data,
         tag: Data,
         aad: Data = Data()
-        ) throws -> Data {
-        
+    ) throws -> Data {
         let (alg, enc) = try guardAlgorithms(header)
-        
+
         guard let staticPrivKey = key as? ECPrivateKey else {
             throw ECDHEESError.invalidJWK(reason: "key must be an ECPrivateKey")
         }
         guard let ephemeralPubKey = header.epk else {
             throw ECDHEESError.invalidJWK(reason: "missing ephemeral public key in header")
         }
-        
+
         let apu = Data(base64Encoded: header.apu ?? "") ?? Data()
         let apv = Data(base64Encoded: header.apv ?? "") ?? Data()
-        
+
         let kek = try keyAgreementCompute(alg, enc, staticPrivKey, ephemeralPubKey, apu, apv)
-        
+
         var cek: Data
         if let keyWrapAlgorithm = alg.keyWrapAlgorithm {
             cek = try keyWrapAlgorithm.unwrap(kek: kek, wrappedKey: encryptedKey)
         } else {
             cek = kek
         }
-        
+
         let decryptionAad = header.jsonSerializedData().base64URLEncodedData()
             + ((aad.count > 0) ? ".".data(using: .ascii)! + aad : aad)
-        
+
         let ciphertext = try enc.decrypt(ciphertext: ciphertext, key: cek, iv: iv, tag: tag, aad: decryptionAad)
-        
-        // ZIP
-        
+
+        if header["zip"] != nil {
+            guard
+                let zip = header["zip"] as? String,
+                let zipAlg = JSONWebEncryptionCompressionAlgorithm(rawValue: zip) else {
+                throw ECDHEESError.unknownOrUnsupportedCompressionAlgorithm(reason: header["zip"] as? String ?? "non String")
+            }
+            return try zipAlg.decompress(ciphertext)
+        }
+
         return ciphertext
     }
 }
--- Example/Tests/DeflateSpec.swift
+++ Example/Tests/DeflateSpec.swift
@@ -13,11 +13,12 @@ import Quick
 class DeflateSpec: QuickSpec {
     override func spec() {
         describe("compress and decompress") {
-            it("should ok") {
+            it("test case from ietf-jose/cookbook/jwe/5_9.compressed_content.json should ok") {
+                
                 let data = "You can trust us to stick with you through thick and thin–to the bitter end. And you can trust us to keep any secret of yours–closer than you keep it yourself. But you cannot trust us to let you face trouble alone, and go off without a word. We are your friends, Frodo.".data(using: .utf8)!
 
                 let expectedCompressed = "bY_BDcIwDEVX-QNU3QEOrIA4pqlDokYxchxVvbEDGzIJbioOSJwc-f___HPjBu8KVFpVtAplVE1-wZo0YjNZo3C7R5v72pV5f5X382VWjYQpqZKAyjziZOr2B7kQPSy6oZIXUnDYbVKN4jNXi2u0yB7t1qSHTjmMODf9QgvrDzfTIQXnyQRuUya4zIWG3vTOdir0v7BRHFYWq3k1k1A_gSDJqtcBF-GZxw8"
-                
+
                 print("length: ", data.count)
                 let compressed = try! deflateCompress(data)
                 expect(compressed.base64URLEncodedString()) == expectedCompressed
